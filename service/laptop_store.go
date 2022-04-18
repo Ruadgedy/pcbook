0c3f8846e1f29298e9a15520743bd6a979eddbc0
Ruadgedy <596318128@qq.com>
2022-04-18 09:25:42 +0800

    新增PC存储类型

---
+++ service/laptop_store.go
@@ -0,0 +1,71 @@
+package service
+
+import (
+	"fmt"
+	"github.com/Ruadgedy/pcbook/pb"
+	"github.com/jinzhu/copier"
+	"sync"
+)
+
+var ErrAlreadyExists = fmt.Errorf("the laptop ID is already exists")
+
+type LaptopStore interface {
+	Save(laptop *pb.Laptop) error
+	Find(id string) (*pb.Laptop, error)
+}
+
+type InMemoryLaptopStore struct {
+	mutex sync.RWMutex
+	// map的key是电脑id，value是电脑对象
+	data map[string]*pb.Laptop
+}
+
+func NewInMemoryLaptopStore() *InMemoryLaptopStore {
+	return &InMemoryLaptopStore{
+		data: make(map[string]*pb.Laptop),
+	}
+}
+
+func (store *InMemoryLaptopStore) Save(laptop *pb.Laptop) error {
+	store.mutex.Lock()
+	defer store.mutex.Unlock()
+
+	if store.data[laptop.Id] != nil {
+		return ErrAlreadyExists
+	}
+
+	// deep copy
+	other := &pb.Laptop{}
+	err := copier.Copy(other, laptop)
+	if err != nil {
+		return fmt.Errorf("cannot copy laptop data: %v", err)
+	}
+
+	store.data[other.Id] = other
+	return nil
+}
+
+func (store *InMemoryLaptopStore) Find(id string) (*pb.Laptop, error) {
+	store.mutex.RLock()
+	defer store.mutex.RUnlock()
+
+	laptop := store.data[id]
+	if laptop == nil {
+		return nil, nil
+	}
+
+	// deep copy
+	other := &pb.Laptop{}
+	err := copier.Copy(other, laptop)
+	if err != nil {
+		return nil, fmt.Errorf("cannot copy laptop data: %w", err)
+	}
+	return other, nil
+}
+
+type DBLaptopStore struct {
+}
+
+func (store *DBLaptopStore) Save(laptop *pb.Laptop) error {
+	panic("implement me")
+}
